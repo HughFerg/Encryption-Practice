5d270ccbcf2ba26e5a4cb3b77dbe6b327b1b0062
hughferguson <hughferguson25@gmail.com>
2017-06-19 16:27:20 -0400

    Added prime testing func

--- RSAEncryptionPractice.py
+++ RSAEncryptionPractice.py
@@ -20,35 +20,66 @@ def getAscVal():
 
 
 def isPrime(n):
-    count = 0
-    for a in range(2, n - 1):
-        if n % a == 0:
-            count += 1
-    if count == 0:
-        return True
-    else:
-        return False
+    # Miller-Rabin Primality Test
+    twoToThex = float(1)
+    multiplier = float()
 
-# calculates every prime number in range and puts it in list "primeList"
+    multiplier = (n - 1) / (2**twoToThex)
+
+    while (multiplier.is_integer()):
+        print twoToThex
+        print multiplier
+        twoToThex = twoToThex + 1
+        multiplier = (n - 1) / (2**twoToThex)
+
+    twoToThex = twoToThex - 1
+    multiplier = (n - 1) / (2**twoToThex)
+
+    print ("%s = 2^%s * %s" % (n - 1, twoToThex, multiplier))
+
+    # Choosing random number between 1,n-1
+
+    a = random.randint(1, n - 1)
+
+    b = (2 ** multiplier) % n
+
+    print b
+
+    if b == 1 or b == -1:
+        return True
+
+    while (b != 1):
+        b = ((b ** 2) % n)
+        a += 1
+    if b == 1:
+        print ("%s is not prime!" % n)
+    elif b == -1:
+        print("%s is probably prime!" % n)
 
 
 def calcPrimes():
-    primeList = []
-    for c in range(2, 5000):
-        if isPrime(c):
-            primeList.append(str(c))
-    # calls func to selct 2 random prime numbers from primeList
-    getRandomPrimes(primeList)
+    num1 = random.getrandbits(2084)
+    if num1 % 2 == 0:
+        num1 += 1
+    num2 = random.getrandbits(2084)
+    if num2 % 2 == 0:
+        num2 += 1
+    n = num1 * num2
 
+    phiOfn = (num1 - 1) * (num2 - 1)
 
-def getRandomPrimes(primeList):
-    print primeList[random.randint(0, len(primeList) - 1)]
-    print primeList[random.randint(0, len(primeList) - 1)]
+    e = 17
+    print num1
+    print num2
+    print ("n =  %s" % n)
+    print ("Phi of n = %s" % phiOfn)
+    print ("e = %s" % e)
 
 
 def main():
     getAscVal()
     calcPrimes()
+    isPrime(561)
 
 
 if __name__ == "__main__":
