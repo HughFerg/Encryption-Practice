5a6c7e1db412338d3659b8b7bf975584df6e39a1
hughferguson <hughferguson25@gmail.com>
2017-06-19 21:00:19 -0400

    Added Miller-Rabin prime functionality

--- RSAEncryptionPractice.py
+++ RSAEncryptionPractice.py
@@ -3,6 +3,7 @@ To be created in conjunction with https://www.youtube.com/watch?v=wXB-V_Keiu8
 RSA explination
 '''
 import random
+import time
 
 
 def getAscVal():
@@ -19,42 +20,42 @@ def getAscVal():
 # checks to see if number is prime
 
 
-def isPrime(n):
-    # Miller-Rabin Primality Test
-    twoToThex = float(1)
-    multiplier = float()
+def isPrime(n, k):
 
-    multiplier = (n - 1) / (2**twoToThex)
+    lowPrimes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587, 593, 599, 601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691, 701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797, 809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883, 887, 907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997]
 
-    while (multiplier.is_integer()):
-        print twoToThex
-        print multiplier
-        twoToThex = twoToThex + 1
-        multiplier = (n - 1) / (2**twoToThex)
+    if n in lowPrimes:
+        return True
+    for prime in lowPrimes:
+        if (n % prime == 0):
+            return False
 
-    twoToThex = twoToThex - 1
-    multiplier = (n - 1) / (2**twoToThex)
-
-    print ("%s = 2^%s * %s" % (n - 1, twoToThex, multiplier))
-
-    # Choosing random number between 1,n-1
-
-    a = random.randint(1, n - 1)
-
-    b = (2 ** multiplier) % n
-
-    print b
-
-    if b == 1 or b == -1:
+    if n == 2:
         return True
 
-    while (b != 1):
-        b = ((b ** 2) % n)
-        a += 1
-    if b == 1:
-        print ("%s is not prime!" % n)
-    elif b == -1:
-        print("%s is probably prime!" % n)
+    if n % 2 == 0:
+        return False
+
+    # Miller-Rabin Primality Test
+
+    r = 0
+    s = n - 1
+
+    while s % 2 == 0:
+        r += 1
+        s //= 2
+    for _ in xrange(k):
+        a = random.randrange(2, n - 1)
+        x = pow(a, s, n)
+        if x == 1 or x == n - 1:
+            continue
+        for _ in xrange(r - 1):
+            x = pow(x, 2, n)
+            if x == n - 1:
+                break
+        else:
+            return False
+    return True
 
 
 def calcPrimes():
@@ -64,6 +65,12 @@ def calcPrimes():
     num2 = random.getrandbits(2084)
     if num2 % 2 == 0:
         num2 += 1
+
+    while not isPrime(num1, 400):
+        num1 += 1
+    while not isPrime(num2, 400):
+        num2 += 1
+
     n = num1 * num2
 
     phiOfn = (num1 - 1) * (num2 - 1)
@@ -78,8 +85,9 @@ def calcPrimes():
 
 def main():
     getAscVal()
+    startTime = time.time()
     calcPrimes()
-    isPrime(561)
+    print (time.time() - startTime)
 
 
 if __name__ == "__main__":
